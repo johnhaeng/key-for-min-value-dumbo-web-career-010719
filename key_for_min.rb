cebe7f67a7ab492a390b54e39cde895ee1b39f05
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-16 21:54:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(name_hash)
     return nil
   end
   
-  min_key = hash.first[0]
-  min_value = hash.first[1]
+  min_key = nil
+  min_value = nil
   hash.each do |key, value|
     if value < min_value
       min_value = value
