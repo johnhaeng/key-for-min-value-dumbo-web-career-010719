a5600b854aa58dd893bca970dd197256bd8f38b9
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-16 21:55:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,7 +12,8 @@ def key_for_min_value(name_hash)
     if value < min_value
       min_value = value
       min_key = key
-    elsif 
+    elsif min_value == nil
+      min_value = value
     end
   end
 
